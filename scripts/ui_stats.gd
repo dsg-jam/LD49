b292ed994fd13fe884b59483e88df44bcbea4dd2
mineiwik <29404520+mineiwik@users.noreply.github.com>
2021-10-02 20:51:56 +0200

    Fix conflicts

--- scripts/ui_stats.gd
+++ scripts/ui_stats.gd
@@ -8,11 +8,12 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	update_stats()
 
 func update_stats():
 	for child in get_children():
-		child.set_fill_degree()
+		for s in child.get_children():
+			s.set_progress_texture(3)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
