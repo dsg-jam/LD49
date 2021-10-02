a147d1ce2e9b6d0f708d075c6510980b24efee86
Simon Berger <simon@siku2.io>
2021-10-02 17:56:15 +0200

    add event database

--- scripts/events/event.gd
+++ scripts/events/event.gd
@@ -1,1 +1,11 @@
-class_name Event
+class_name LGameEvent
+
+var gid: int
+var name: String
+
+func execute_consequence() -> void:
+	push_error("event without consequence implementation")
+
+func from_dict(data: Dictionary) -> void:
+	self.gid = data["gid"] as int
+	self.name = data["name"] as String
