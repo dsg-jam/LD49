dfab97ceb68c46c1f46cf0125284362c2a30f587
mineiwik <29404520+mineiwik@users.noreply.github.com>
2021-10-02 16:49:01 +0200

    Add stats class (#11)

--- scripts/stats.gd
+++ scripts/stats.gd
@@ -1,1 +1,15 @@
-class_name Stats
+var name: String
+var value: int
+
+
+func _init(name: String, value: int = 100):
+	self.name = name
+	self.value = value
+
+
+func change_value(amount: int):
+	self.value += amount
+	if self.value > 100:
+		self.value = 100
+	elif self.value < 0:
+		self.value = 0
