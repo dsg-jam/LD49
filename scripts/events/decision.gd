4af7ba25f6f21cb474895c3aee6fda2c433a0cf0
Simon Berger <simon@siku2.io>
2021-10-02 17:56:15 +0200

    add event database

--- scripts/events/decision.gd
+++ scripts/events/decision.gd
@@ -1,2 +1,19 @@
 class_name Decision
-extends Event
+extends LGameEvent
+
+const Option := preload("res://scripts/events/decision_option.gd")
+
+var options: Array
+
+func execute_consequence() -> void:
+	# TODO implement using state
+	pass
+
+func from_dict(data: Dictionary) -> void:
+	.from_dict(data)
+
+	self.options = []
+	for option in data["options"]:
+		var opt := Option.new()
+		opt.from_dict(option)
+		self.options.append(opt)
