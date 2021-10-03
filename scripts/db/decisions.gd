541c628d424cef131899f4eb06a0762c11b20131
thieme55 <thieme55@users.noreply.github.com>
2021-10-03 20:38:16 +0200

    adds the first events

--- scripts/db/decisions.gd
+++ scripts/db/decisions.gd
@@ -1,54 +1,67 @@
 const RAW := [{
-	"gid": 1,
-	"name": "The first test",
-	"prompt": "Oh lord, stuff happened and now some more stuff happenend. What will you do?",
+	"gid": 0,
+	"name": "Introduction",
+	"prompt": "You are the ruler of this unstable country. Your task is to lead the country through this difficult time. Make one of these decisions. But be careful, decisions can influence your stats [see the right figure]... /n Today, your minister of inner affairs made you aware, that more and more people are losing their job. Those who are now suffering are threatening to revolt against you. What will you do?",
 	"options": [{
-		"text": "Let's just literally kill everyone",
-		"id": "kill",
+		"text": "Invest in infrastructure",
+		"id": "infrastructure",
 		"consequences": {
-			"military": 20,
-			"stability": -10,
+			"stability": 1,
+			"money": -10,
 		},
 	}, {
-		"text": "Figure it out yourselves",
+		"text": "Start a propaganda agency",
 		"consequences": {
-			"military": -20,
-			"stability": 10,
+			"money": -5,
+			"stability": 5,
 		},
 	}, {
-		"text": "...",
+		"text": "Ignore for now",
 		"consequences": {
-			"money": 5,
+			"stability": -5,
 		},
 	}],
 }, {
-	"gid": 2,
-	"name": "The second test",
-	"prompt": "Some more stuff happened",
+	"gid": 1,
+	"name": "military at the blue boarder",
+	"prompt": "Your best Diplomat comes to you and explains, that the leader of the country in the east (blue) has made you an ultimatum to pay back your long-lasting dept. He threatens to set sanctions on all trades concerning military equipment in place.",
 	"options": [{
-		"text": "Ignore it",
+		"text": "Start paying back some of your dept.",
 		"consequences": {
-			"stability": -10,
-			"money": 30,
+			"diplomacy": 5,
+			"money": -30,
 		},
 	}, {
-		"text": "Deal with it",
+		"text": "Increase military personnel on the boarder to the blue country.",
 		"consequences": {
-			"diplomacy": 20,
+			"diplomacy": -20,
+			"money": -5,
+			"stability": -5,
+			"military": 5,
+		},
+	},{
+		"text": "Ignore it.",
+		"consequences": {
+			"military": -15,
 		},
 	}],
 }, {
 	"gid": 3,
-	"name": "The third test",
-	"prompt": "I don't even know anymore",
-	"requirements": [{
-		"gid": 1,
-		"chosen_option_id": "kill",
-	}],
+	"name": "A bird war",
+	"prompt": "A bird told me that one of the neighboring countries may be planning a war against you. What are you going to do about it?",
 	"options": [{
-		"text": "Take the blame",
+		"text": "Upgrade your army",
 		"consequences": {
-			"stability": -50,
+			"diplomacy": -15,
+			"money": -30,
+			"military": 20,
 		},
-	}],
+	},{
+		"text": "Ignore this bird",
+		"id": "ignore_bird",
+		"consequences": {
+			"stability": -15,
+			"military": -25,
+		},
+	},],
 }]
