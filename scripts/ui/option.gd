798629a7b63b91f04591703374e33d489c29c6a2
Simon Berger <simon@siku2.io>
2021-10-03 15:30:58 +0200

    remove unnecessary signal

--- scripts/ui/option.gd
+++ scripts/ui/option.gd
@@ -1,11 +1,6 @@
 extends Control
 
-signal pressed
-
 onready var label := $Label
 
 func setup(option: LGameDecision.Option) -> void:
 	label.text = option.text
-
-func _on_Button_pressed() -> void:
-	emit_signal("pressed")
