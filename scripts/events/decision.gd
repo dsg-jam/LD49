ade87b00cec020434d0f09b5c9e745e2c77e4c01
Simon Berger <simon@siku2.io>
2021-10-03 17:36:22 +0200

    move requirements to base event

--- scripts/events/decision.gd
+++ scripts/events/decision.gd
@@ -18,59 +18,9 @@ class Option:
 			assert(stat is String)
 			assert(self.consequences[stat] is int)
 
-class Requirement:
-	var gid: int
-	var option_id: String
-
-	# String -> int (stat -> absolute value)
-	var min_stats: Dictionary
-	# String -> int (stat -> absolute value)
-	var max_stats: Dictionary
-
-	func matches(event: LGameEvent, stats: Dictionary) -> bool:
-		if self.gid != -1 && self.gid != event.gid:
-			return false
-		if self.option_id:
-			var decision := event as LGameDecision
-			if decision != null and self.option_id != decision.get_chosen_option().id:
-				return false
-		for stat in self.min_stats:
-			if stats[stat] < self.min_stats[stat]:
-				return false
-		for stat in self.max_stats:
-			if stats[stat] > self.max_stats[stat]:
-				return false
-		return true
-
-	func from_dict(data: Dictionary) -> void:
-		if "gid" in data:
-			self.gid = data["gid"] as int
-			self.option_id = data["option_id"] as String
-		else:
-			self.gid = -1
-			self.option_id = ""
-
-		if "min_stats" in data:
-			self.min_stats = data["min_stats"]
-			for stat in self.min_stats:
-				assert(stat is String)
-				assert(self.min_stats[stat] is int)
-		else:
-			self.min_stats = {}
-
-		if "max_stats" in data:
-			self.max_stats = data["max_stats"]
-			for stat in self.max_stats:
-				assert(stat is String)
-				assert(self.max_stats[stat] is int)
-		else:
-			self.max_stats = {}
-
 var prompt: String
 # Option[]
 var options: Array
-# Requirement[]
-var requirements: Array
 
 var chosen_option: int = -1
 
@@ -100,9 +50,3 @@ func from_dict(data: Dictionary) -> void:
 		self.options.append(opt)
 	assert(self.options.size() > 0, "must provide at least one option")
 
-	self.requirements = []
-	if "requirements" in data:
-		for raw_requirement in data["requirements"]:
-			var requirement := Requirement.new()
-			requirement.from_dict(raw_requirement)
-			self.requirements.append(requirement)
