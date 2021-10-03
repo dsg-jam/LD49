70a50077e171a5dc110fb32813d69a4c3cc44f50
Simon Berger <simon@siku2.io>
2021-10-03 18:22:43 +0200

    send eod events to overview

--- scripts/overview.gd
+++ scripts/overview.gd
@@ -6,6 +6,8 @@ onready var day_label := $Day
 func _ready() -> void:
 	var err := Manager.connect("decision_started", self, "_on_decision_started")
 	assert(err == OK)
+	err = Manager.connect("eod_event_started", self, "_on_eod_event_started")
+	assert(err == OK)
 	err = Manager.connect("day_changed", self, "_on_day_changed")
 	assert(err == OK)
 
@@ -13,10 +15,14 @@ func _ready() -> void:
 
 func _on_decision_started(p_decision: LGameDecision) -> void:
 	decision.setup(p_decision)
-	
+
+func _on_eod_event_started(event: LGameEodEvent) -> void:
+	print("WE NEED TO DISPLAY THIS EVENT TO THE USER: ", event)
+	Manager.finish_event(event)
+
 func _on_day_changed(day: int) -> void:
 	day_label.text = "DAY %d" % (day + 1)
 
 func _on_Decision_selected(p_decision: LGameDecision) -> void:
 	decision.visible = false
-	Manager.finish_decision(p_decision)
+	Manager.finish_event(p_decision)
